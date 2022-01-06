f1a651c1ebaabea63e0a9982f050dc99850e3dee
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-06 12:38:57 +0530

    optimized code with closures

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,27 @@ pub struct Config {
 }
 
 impl Config{
-        pub fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
     
-            if args.len() < 3 {
-                return Err("Not enough arguments!");
-            }
-    
-            let query = args[1].clone();
-            let filename = args[2].clone();
+        args.next();
 
-            let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-    
-            Ok(Config { 
-                query, 
-                filename, 
-                case_sensitive 
-            })
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
